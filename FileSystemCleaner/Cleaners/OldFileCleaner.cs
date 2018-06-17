57a12355342689a05f4e98f1933e89a11f78c711
drummk2 <kieron.drumm2@mail.dcu.ie>
2018-06-17 20:30:52 +0100

    reddinc3 - Adding code for OldFileCleaner.cs

--- FileSystemCleaner/Cleaners/OldFileCleaner.cs
+++ FileSystemCleaner/Cleaners/OldFileCleaner.cs
@@ -1,4 +1,7 @@
 ﻿using FileSystemCleaner.Bases;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace FileSystemCleaner.Cleaners
 {
@@ -6,7 +9,38 @@ namespace FileSystemCleaner.Cleaners
     {
         public override void Init(string currentDir, bool isQuiet)
         {
-            throw new System.NotImplementedException();
+            Console.WriteLine("Deleting old files");
+            Clean(currentDir, isQuiet);
+        }
+
+        private void Clean(string currentDir, bool isQuiet)
+        {
+            new List<string>(Directory.GetDirectories(currentDir)).ForEach(dir => Clean(dir, isQuiet));
+
+            new List<string>(Directory.EnumerateFiles(currentDir)).ForEach(file => {
+                if (DateTime.Now - File.GetLastWriteTime(file) > TimeSpan.FromDays(1))
+                {
+                    if (isQuiet)
+                    {
+                        try
+                        {
+                            File.Delete(file);
+                        }
+                        catch (IOException) { }
+                    }
+                    else
+                    {
+                        if (Utilities.GetDeleteConfirmation(file))
+                        {
+                            try
+                            {
+                                File.Delete(file);
+                            }
+                            catch (IOException) { }
+                        }
+                    }
+                }
+            });
         }
     }
 }
